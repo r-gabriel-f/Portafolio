8f390b964fa541e381cdbed95f0cf360be878357
robert <r.gabriel.f@outlook.com>
2023-05-10 23:09:27 -0400

    sobre mi ya acabado mis habilidades aun falta

--- src/components/About.js
+++ src/components/About.js
@@ -1,23 +1,34 @@
-import React from 'react'
-import './About.css'
+import React from "react";
+import "./About.css";
+import Button from "@mui/material/Button";
+import cv from "../assets/cv/gabriel.pdf";
 export const About = () => {
   return (
-    <section className='container-about'>
-        <div>
-            <h1>Sobre Mi</h1>
-            <h3>Soy Robert Gabriel, Desarrollador Web/ Ingeniero de Sistemas</h3>
-            <p>Hola, mi nombre es Robert Gabriel y soy un apasionado 
-                desarrollador front-end con experiencia en 
-                desarrollo de sitios web modernos y responsivos. 
-                Me encanta trabajar en proyectos que desafíen mis 
-                habilidades y me permitan aprender nuevas 
-                tecnologías y herramientas.</p>
-            
+    <section className="container-about">
+      <div className="container-div">
+        <h1 id="h1">Sobre Mi</h1>
+        <h3 id="h3">
+          Soy Robert Gabriel, Desarrollador Web/ Ingeniero de Sistemas
+        </h3>
+        <p id="p">
+          Hola, mi nombre es Robert Gabriel y soy un apasionado desarrollador
+          front-end con experiencia en desarrollo de sitios web modernos y
+          responsivos. Me encanta trabajar en proyectos que desafíen mis
+          habilidades y me permitan aprender nuevas tecnologías y herramientas.
+        </p>
+        <div id="download">
+          <a href={cv} download="Robert_Gabriel_CV.pdf">
+            <Button variant="contained">Descargar CV</Button>
+          </a>
         </div>
-        
-        <div>
+      </div>
+      <div className="container-icon">
         <h1>Mis Habilidades</h1>
+        <div>
+          <h1>HTML5</h1>
+          <ion-icon name="logo-html5" id="html-icon"></ion-icon>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
