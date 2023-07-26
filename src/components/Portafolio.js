b30f014856ed77f1dcc88f09404292b7bcd598ed
robert <r.gabriel.f@outlook.com>
2023-07-26 14:03:04 -0400

    proyecto2 agregado

--- src/components/Portafolio.js
+++ src/components/Portafolio.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../components/Portafolio.css";
 import t1 from "../assets/img/t1.png";
+import t2 from "../assets/img/t2.PNG";
 import Button from "@mui/material/Button";
 export const Portafolio = () => {
   return (
@@ -30,6 +31,34 @@ export const Portafolio = () => {
                   </a>
                   <a
                     href="https://github.com/r-gabriel-f/empresa_minera_huanuni"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Button variant="contained" id="bp">
+                    Repositorio
+                    </Button>
+                  </a>
+                  
+                </div>
+              </div>
+            </div>
+            <div className="image-container">
+              <img src={t2} id="img-t1" alt="Icono de responsivo"></img>
+              <div class="button-container">
+                <p>Country Api React</p>
+
+                <div className="botones-portafolio">
+                  <a
+                    href="https://r-gabriel-f.github.io/country_api_react/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Button variant="contained" id="bp">
+                      Sitio Web
+                    </Button>
+                  </a>
+                  <a
+                    href="https://github.com/r-gabriel-f/country_api_react"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
