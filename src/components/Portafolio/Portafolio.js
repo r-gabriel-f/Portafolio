8bce52d9c98ec2273fd55b533df2519ac001333c
robert <r.gabriel.f@outlook.com>
2023-09-25 15:57:48 -0400

    se creo el array de proyectos

--- src/components/Portafolio/Portafolio.js
+++ src/components/Portafolio/Portafolio.js
@@ -1,11 +1,8 @@
 import React from "react";
 import "../Portafolio/Portafolio.css";
-import t1 from "../../assets/img/t1.webp";
-import t2 from "../../assets/img/t2.webp";
-import t3 from "../../assets/img/t3.webp";
-import t4 from "../../assets/img/t4.webp";
-import t5 from "../../assets/img/t5.webp";
 import Button from "@mui/material/Button";
+
+import { DataPortafolio } from "../../data/DataPortafolio";
 export const Portafolio = () => {
   return (
     <section className="container-portafolio">
@@ -17,140 +14,38 @@ export const Portafolio = () => {
 
         <div className="iconos-portafolio">
           <div className="portafolio-div">
-            <div className="image-container">
-              <p>Landing Pages React: E.M.H</p>
-              <img src={t1} id="img-t1" alt="Icono de responsivo"></img>
-              <div className="button-container">
-                <div className="botones-portafolio">
-                  <a
-                    href="https://r-gabriel-f.github.io/empresa_minera_huanuni/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Button variant="contained" id="bp">
-                      Sitio Web
-                    </Button>
-                  </a>
-                  <a
-                    href="https://github.com/r-gabriel-f/empresa_minera_huanuni"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Button variant="contained" id="bp">
-                      Repositorio
-                    </Button>
-                  </a>
+            {DataPortafolio.map((proyect, i) => (
+              <div key={i} className="image-container">
+                <p>{proyect.name}</p>
+                <img
+                  src={proyect.img}
+                  alt={proyect.name}
+                  id="img-t1"
+                ></img>
+                <div className="button-container">
+                  <div className="botones-portafolio">
+                    <a
+                      href={proyect.web}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button variant="contained" id="bp">
+                        Sitio Web
+                      </Button>
+                    </a>
+                    <a
+                      href={proyect.repositori}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button variant="contained" id="bp">
+                        Repositorio
+                      </Button>
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="image-container">
-              <p>Country Api React</p>
-
-              <img src={t2} id="img-t1" alt="Icono de responsivo"></img>
-              <div className="button-container">
-                <div className="botones-portafolio">
-                  <a
-                    href="https://r-gabriel-f.github.io/country_api_react/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Button variant="contained" id="bp">
-                      Sitio Web
-                    </Button>
-                  </a>
-                  <a
-                    href="https://github.com/r-gabriel-f/country_api_react"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Button variant="contained" id="bp">
-                      Repositorio
-                    </Button>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="image-container">
-              <p>Movie Api React</p>
-
-              <img src={t3} id="img-t1" alt="Icono de responsivo"></img>
-              <div className="button-container">
-                <div className="botones-portafolio">
-                  <a
-                    href="https://r-gabriel-f.github.io/desafio/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Button variant="contained" id="bp">
-                      Sitio Web
-                    </Button>
-                  </a>
-                  <a
-                    href="https://github.com/r-gabriel-f/desafio"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Button variant="contained" id="bp">
-                      Repositorio
-                    </Button>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="image-container">
-              <p>Pokemon Api React</p>
-
-              <img src={t4} id="img-t1" alt="Icono de responsivo"></img>
-              <div className="button-container">
-                <div className="botones-portafolio">
-                  <a
-                    href="https://r-gabriel-f.github.io/pokemon/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Button variant="contained" id="bp">
-                      Sitio Web
-                    </Button>
-                  </a>
-                  <a
-                    href="https://github.com/r-gabriel-f/pokemon"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Button variant="contained" id="bp">
-                      Repositorio
-                    </Button>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="image-container">
-              <p>Shopping cart</p>
-
-              <img src={t5} id="img-t1" alt="Icono de responsivo"></img>
-              <div className="button-container">
-                <div className="botones-portafolio">
-                  <a
-                    href="https://r-gabriel-f.github.io/carrito_de_compras/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Button variant="contained" id="bp">
-                      Sitio Web
-                    </Button>
-                  </a>
-                  <a
-                    href="https://github.com/r-gabriel-f/carrito_de_compras"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Button variant="contained" id="bp">
-                      Repositorio
-                    </Button>
-                  </a>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
---
+++ src/data/DataPortafolio.js
@@ -0,0 +1,45 @@
+import t1 from "../assets/img/t1.webp";
+import t2 from "../assets/img/t2.webp";
+import t3 from "../assets/img/t3.webp";
+import t4 from "../assets/img/t4.webp";
+import t5 from "../assets/img/t5.webp";
+export const DataPortafolio = [
+    {
+      id: 1,
+      name: "Landing Pages React: E.M.H",
+      img: t1,
+      web: "https://r-gabriel-f.github.io/empresa_minera_huanuni/",
+      repositori: "https://github.com/r-gabriel-f/empresa_minera_huanuni",
+    },
+    {
+      id: 2,
+      name: "Country Api React",
+      img: t2,
+      web: "https://r-gabriel-f.github.io/country_api_react/",
+      repositori: "https://github.com/r-gabriel-f/country_api_react",
+    },
+    {
+      id: 3,
+      name: "Movie Api React",
+      img: t3,
+      web: "https://r-gabriel-f.github.io/desafio/",
+      repositori: "https://github.com/r-gabriel-f/desafio",
+    },
+    {
+      id: 4,
+      name: "Pokemon Api React",
+      img: t4,
+      web: "https://r-gabriel-f.github.io/pokemon/",
+      repositori: "https://github.com/r-gabriel-f/pokemon",
+    },
+  
+    {
+      id: 5,
+      name: "Shopping cart",
+      img: t5,
+      web: "https://r-gabriel-f.github.io/carrito_de_compras/",
+      repositori: "https://github.com/r-gabriel-f/carrito_de_compras",
+    },
+   
+  ];
+  
