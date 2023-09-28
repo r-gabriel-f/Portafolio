217c9e0cb2480e7897810160176d9f90e1832abd
robert <r.gabriel.f@outlook.com>
2023-09-28 11:50:41 -0400

    se realizo el cambio de inicio

--- src/components/Inicio/Inicio.js
+++ src/components/Inicio/Inicio.js
@@ -1,24 +1,20 @@
 import { React } from "react";
 import "../Inicio/Inicio.css";
-import person from "../../assets/img/yo.webp";
-import code from "../../assets/img/codigo.webp";
+import person from "../../assets/img/yo.jpeg";
 
 export const Inicio = () => {
   return (
-    <section id="inicio" className="inicio">
-      <div className="container-banner">
+    <div className="container-banner">
+      <div className="container-inner">
         <div className="container-img">
           <img src={person} alt="Icono de responsivo" />
         </div>
         <div className="container-info">
-          <h2>Hola</h2>
+          <h2>¡Hola!</h2>
           <h1>Soy Robert Gabriel</h1>
-          <h2>Desarrollador Front End</h2>
-        </div>
-        <div className="container-img-codi">
-          <img src={code} alt="Icono de responsivo" />
+          <h2>Frontend Developer</h2>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
