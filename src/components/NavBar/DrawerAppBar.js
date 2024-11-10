a52b68248bf138a23d25b04f5cc9e9ffc584329f
robert <robert.flores@shipedge.com>
2024-11-10 15:59:07 -0400

    navbar correction

--- src/components/NavBar/DrawerAppBar.js
+++ src/components/NavBar/DrawerAppBar.js
@@ -1,139 +1,51 @@
 import * as React from "react";
-import PropTypes from "prop-types";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
-import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import { Link } from "react-scroll";
+import { Menubar } from "primereact/menubar";
 
-const drawerWidth = 240;
-const navItems = ["INICIO", "SOBRE MI", "SERVICIOS","PORTAFOLIO", "CONTACTOS"];
-function DrawerAppBar(props) {
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
 
-  const handleDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState);
-  };
+function DrawerAppBar() {
+  const items = [
+    {
+      label: "Inicio",
+      icon: "pi pi-home",
 
-  
+    },
+    {
+      label: "Sobre Mi",
+      icon: "pi pi-user",
 
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-      <Divider />
-      <List>
-        {navItems.map((item) => (
-          <ListItem key={item} disablePadding>
-            <ListItemButton sx={{ justifyContent: 'center' }}>
-              <Link
-                to={item.toLowerCase().replace(' ', '-')}
-                spy={true}
-                smooth={true}
-                duration={500}
-                offset={-70}
-                className="nav-link"
-                onClick={handleDrawerToggle}
-              >
-                {item}
-              </Link>
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
+    },
+    {
+      label: "Servicios",
+      icon: "pi pi-cog",
+
+    },
+    {
+      label: "Portafolio",
+      icon: "pi pi-images",
+
+    },
+    {
+      label: "Contactos",
+      icon: "pi pi-phone",
+
+    },
+  ];
+
+  const end = (
+    <div className="flex items-center gap-2 mr-2">
+      <i className="pi pi-code mx-2" style={{ color: "var(--primary-color)" }}></i>
+      RG
+    </div>
   );
-  
-  const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar component="nav" sx={{ backgroundColor: '#25292B' }}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{
-              flexGrow: 1,
-              mr: 2,
-              textAlign: { xs: 'center', sm: 'left' },
-              my: { xs: 2, sm: 0 },
-            }}
-          >
-            <ion-icon
-              name="code-working-outline"
-              id="logo-code"
-              style={{
-                fontSize: '2rem',
-                verticalAlign: 'middle',
-                marginRight: '0.5rem',
-              }}
-            ></ion-icon>
-            RG.
-          </Typography>
-
-          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-            {navItems.map((item) => (
-              <Button key={item} sx={{ color: '#fff' }}>
-                <Link
-                  to={item.toLowerCase().replace(' ', '-')}
-                  spy={true}
-                  smooth={true}
-                  duration={500}
-                  offset={-70}
-                  className="nav-link"
-                >
-                  {item}
-                </Link>
-              </Button>
-            ))}
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        container={container}
-        variant="temporary"
-        open={mobileOpen}
-        onClose={handleDrawerToggle}
-        ModalProps={{
-          keepMounted: true,
-        }}
-        sx={{
-          display: { xs: 'block', sm: 'none' },
-          '& .MuiDrawer-paper': {
-            boxSizing: 'border-box',
-            width: drawerWidth,
-            backgroundColor: '#25292B',
-            color: 'white',
-          },
-        }}
-      >
-        {drawer}
-      </Drawer>
-    </Box>
+    <div className="card">
+      <Menubar 
+        model={items} 
+        end={end} 
+      />
+    </div>
   );
 }
-
-DrawerAppBar.propTypes = {
-  window: PropTypes.func,
-};
 
 export default DrawerAppBar;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import App from './App';
+// Primero importa los estilos de PrimeReact
 import 'primereact/resources/themes/lara-light-cyan/theme.css';
+import 'primereact/resources/primereact.min.css';  // Agrega esta línea
 import 'primeicons/primeicons.css';
+// Después importa los estilos de tu aplicación
+import './index.css';
+import App from './App';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
