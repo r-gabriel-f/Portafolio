e06a9acce9255739c10635926bc9f685f5083b7c
robert <robert.flores@shipedge.com>
2024-11-10 19:04:18 -0400

    about correction

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,61 +1,109 @@
 import React from "react";
-import "../About/About.css";
 
 import cv from "../../assets/cv/gabriel.pdf";
 import html from "../../assets/iconos/skills/html.png";
 import js from "../../assets/iconos/skills/js.png";
 import css from "../../assets/iconos/skills/css.png";
 import git from "../../assets/iconos/skills/git.png";
-import angular from "../../assets/iconos/skills/angular.png";
+import tailwind from "../../assets/iconos/skills/tailwind.webp";
+import vue from "../../assets/iconos/skills/vue.webp";
+import ts from "../../assets/iconos/skills/typscript.webp";
 import react from "../../assets/iconos/skills/react.png";
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import { Button } from 'primereact/button';
+import { Button } from "primereact/button";
 export const About = () => {
   return (
-    <section className="container-about">
-      <div className="container-div">
-        <h1 id="h1">Sobre Mi</h1>
-        <h3 id="h3">
-          Soy Robert Gabriel, Desarrollador Front End / Ingeniero de Sistemas
+    <section className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 h-screen">
+      <div className="flex flex-col justify-center space-y-4">
+        <h2 className="text-3xl md:text-4xl font-bold tracking-tight">About</h2>
+        <h3 className="text-xl md:text-2xl">
+          I'm Robert Gabriel
+          <span className="block text-[#4A9DFF]">
+            Front End Developer / Systems Engineer
+          </span>
         </h3>
-        <p id="p">
-          Hola, mi nombre es Robert Gabriel y soy un apasionado desarrollador
-          front-end con experiencia en desarrollo de sitios web modernos y
-          responsivos. Me encanta trabajar en proyectos que desafíen mis
-          habilidades y me permitan aprender nuevas tecnologías y herramientas.
+        <p className=" leading-relaxed">
+          I'm a passionate frontend developer, specialized in creating modern,
+          functional and responsive websites. I'm motivated to face challenges
+          that drive the development of my skills and allow me to adopt new
+          technologies and tools to offer efficient and innovative solutions.
         </p>
-        <div id="download">
+        <div className="flex justify-center">
           <a href={cv} download="CV_Robert_Gabriel.pdf">
-          <Button label="Descargar CV" icon="pi pi-download" />
+            <Button
+              label="Download CV"
+              icon="pi pi-download"
+              severity="secondary"
+            />
           </a>
         </div>
       </div>
-      <div className="container-icon">
-        <h1 className="skills">Mis Habilidades</h1>
-        <div className="iconos">
-          <div>
-            <h3 id="te">HTML</h3>
-            <LazyLoadImage src={html} id="html-icon" alt="Icono de responsivo"></LazyLoadImage>
+
+      <div className="flex flex-col justify-center space-y-4">
+        <h1 className="text-3xl font-bold">Skills</h1>
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+          <div className="flex flex-col items-center">
+            <h3 className="text-base font-semibold">Html</h3>
+            <img
+              src={html}
+              alt="HTML Icon"
+              className="w-16 sm:w-20 md:w-24 hover:rotate-[360deg] transition-transform duration-1000"
+            />
           </div>
-          <div>
-            <h3 id="te">CSS</h3>
-            <LazyLoadImage src={css} id="html-icon" alt="Icono de responsivo"></LazyLoadImage>
+          <div className="flex flex-col items-center">
+            <h3 className="text-base font-semibold">Css</h3>
+            <img
+              src={css}
+              alt="CSS Icon"
+              className="w-16 sm:w-20 md:w-24 hover:rotate-[360deg] transition-transform duration-1000"
+            />
           </div>
-          <div>
-            <h3 id="te">JS</h3>
-            <LazyLoadImage src={js} id="html-icon" alt="Icono de responsivo"></LazyLoadImage>
+          <div className="flex flex-col items-center">
+            <h3 className="text-base font-semibold">Tailwind</h3>
+            <img
+              src={tailwind}
+              alt="Tailwind Icon"
+              className="w-16 sm:w-20 md:w-24 hover:rotate-[360deg] transition-transform duration-1000"
+            />
           </div>
-          <div>
-            <h3 id="te">ANGULAR</h3>
-            <LazyLoadImage src={angular} id="html-icon" alt="Icono de responsivo"></LazyLoadImage>
+          <div className="flex flex-col items-center">
+            <h3 className="text-base font-semibold">JS</h3>
+            <img
+              src={js}
+              alt="JS Icon"
+              className="w-16 sm:w-20 md:w-24 hover:rotate-[360deg] transition-transform duration-1000"
+            />
           </div>
-          <div>
-            <h3 id="te">REACT</h3>
-            <LazyLoadImage src={react} id="html-icon" alt="Icono de responsivo"></LazyLoadImage>
+          <div className="flex flex-col items-center">
+            <h3 className="text-base font-semibold">TS</h3>
+            <img
+              src={ts}
+              alt="TS Icon"
+              className="w-16 sm:w-20 md:w-24 hover:rotate-[360deg] transition-transform duration-1000"
+            />
           </div>
-          <div>
-            <h3 id="te">GIT</h3>
-            <LazyLoadImage src={git} id="html-icon" alt="Icono de responsivo"></LazyLoadImage>
+          <div className="flex flex-col items-center">
+            <h3 className="text-base font-semibold">React</h3>
+            <img
+              src={react}
+              alt="React Icon"
+              className="w-16 sm:w-20 md:w-24 hover:rotate-[360deg] transition-transform duration-1000"
+            />
+          </div>
+          <div className="flex flex-col items-center">
+            <h3 className="text-base font-semibold">Vue</h3>
+            <img
+              src={vue}
+              alt="Vue Icon"
+              className="w-16 sm:w-20 md:w-24 hover:rotate-[360deg] transition-transform duration-1000"
+            />
+          </div>
+          <div className="flex flex-col items-center">
+            <h3 className="text-base font-semibold">Git</h3>
+            <img
+              src={git}
+              alt="Git Icon"
+              className="w-16 sm:w-20 md:w-24 hover:rotate-[360deg] transition-transform duration-1000"
+            />
           </div>
         </div>
       </div>
