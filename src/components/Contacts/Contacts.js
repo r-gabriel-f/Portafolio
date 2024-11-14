3d2a3dde2cba4edfdc262c055317d63b0f1390d5
robert <robert.flores@shipedge.com>
2024-11-13 21:55:19 -0400

    v2 correcion contact

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,67 +1,68 @@
 import React from "react";
-import "../Contacts/Contacts.css";
 
 export default function Contacts() {
   const currentYear = new Date().getFullYear();
   return (
-    <section className="container-contacts">
-      <div className="con-with">
-        <div className="container-div-contactas">
-          <h1>Contactos</h1>
-          <p>
-            Si estás interesado en adquirir mis servicios para desarrollar tu
-            sitio web o aplicación web no dude en contactarnos
-          </p>
-        </div>
-        <div className="container-icons-contacts">
-          <div>
-            <a href="https://twitter.com/robert79757" target="_banks">
-              <ion-icon name="logo-twitter" id="iconos-contacto-tw"></ion-icon>
-              <h3>Twitter</h3>
-            </a>
-          </div>
-          <div>
-            <a href="mailto:r.gabriel.f@outlook.com" target="_banks">
-              <ion-icon
-                name="mail-outline"
-                id="iconos-contacto-email"
-              ></ion-icon>
-              <h3>Email</h3>
-            </a>
-          </div>
-          <div>
-            <a href="https://github.com/r-gabriel-f" target="_banks">
-              <ion-icon name="logo-github" id="iconos-contacto-git"></ion-icon>
-              <h3>Github</h3>
-            </a>
-          </div>
-          <div>
-            <a href="https://wa.link/mvtvgw" target="_banks">
-              <ion-icon name="logo-whatsapp" id="iconos-contacto-sw"></ion-icon>
-              <h3>Whatsapp</h3>
-            </a>
-          </div>
-
-          <div>
-            <a
-              href="https://linkedin.com/in/robert-gabriel-flores-99b903234"
-              target="_banks"
-            >
-              <ion-icon
-                name="logo-linkedin"
-                id="iconos-contacto-link"
-              ></ion-icon>
-              <h3>Linkedin</h3>
-            </a>
-          </div>
-        </div>
-        <footer className="Derechos">
-          <p>
-            &copy; {currentYear} Robert Gabriel. <br />
-            Todos los derechos reservados.
-          </p>
-        </footer>
+    <section className="bg-black bg-fondoContact p-4">
+      <div className="text-white">
+        <h2 className="text-3xl md:text-4xl font-bold tracking-tight">
+          Contacts
+        </h2>
+        <p className="leading-relaxed text-lg">
+          If you are interested in acquiring my services to develop your website
+          or web application, feel free to contact us.
+        </p>
       </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8">
+        <div className="flex flex-col items-center justify-center text-center">
+          <a
+            href="mailto:r.gabriel.f@outlook.com"
+            target="_banks"
+            className="text-neutral-700 hover:text-black no-underline"
+          >
+            <i className="pi pi-envelope text-4xl mb-2 text-yellow-500"></i>
+            <h3 className="text-yellow-500">Email</h3>
+          </a>
+        </div>
+        <div className="flex flex-col items-center justify-center text-center">
+          <a
+            href="https://github.com/r-gabriel-f"
+            target="_banks"
+            className="text-neutral-700 hover:text-black no-underline"
+          >
+            <i className="pi pi-github text-4xl mb-2 text-gray-800"></i>
+            <h3 className="text-gray-800">Github</h3>
+          </a>
+        </div>
+        <div className="flex flex-col items-center justify-center text-center">
+          <a
+            href="https://wa.link/mvtvgw"
+            target="_banks"
+            className="text-neutral-700 hover:text-black no-underline"
+          >
+            <i className="pi pi-whatsapp text-4xl mb-2 text-green-500"></i>
+            <h3 className="text-green-500">Whatsapp</h3>
+          </a>
+        </div>
+        <div className="flex flex-col items-center justify-center text-center">
+          <a
+            href="https://linkedin.com/in/robert-gabriel-flores-99b903234"
+            target="_banks"
+            className="text-neutral-700 hover:text-black no-underline"
+          >
+            <i className="pi pi-linkedin text-4xl mb-2 text-blue-700"></i>
+            <h3 className="text-blue-700">Linkedin</h3>
+          </a>
+        </div>
+      </div>
+
+      <footer className="mt-8 text-center text-white">
+        <p>
+          &copy; {new Date().getFullYear()} Robert Gabriel. <br />
+          Todos los derechos reservados.
+        </p>
+      </footer>
     </section>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   important: true,
   theme: {
     extend: {
       backgroundImage: {
-        'fondo': "url('../src/assets/img/fondo.webp')",
+        fondo: "url('../src/assets/img/fondo.webp')",
+        fondoContact:
+          "url('https://www.transparenttextures.com/patterns/black-mamba.png')",
       },
     },
   },
   plugins: [],
   corePlugins: {
-    preflight: false
-  }
-}
+    preflight: false,
+  },
+};
